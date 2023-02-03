4fbded273282533684f76f4a71b6a656661f1d30
GastII <gpaschetta17@gmail.com>
2023-02-03 14:55:47 -0300

    2ndLucid

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # JS
+pusheado prro
--- funciones.js
+++ funciones.js
@@ -1,1 +1,36 @@
-como q nada to commit
+
+
+function sumaUno(x) {
+    return x + 10;
+}
+
+
+
+var sumaDos = function (x) {
+    return x (-3);
+}
+
+
+var sumaTres = (x) => {
+    return x + 1;
+};
+
+sumaUno(3);
+sumaDos(5);
+sumaTres(7);
+
+//Función: sumaTres
+var sumaTres = (x) => {
+    return x + 3;
+  }
+  
+  //Función: cuidadoConElConsoleLog
+  function cuidadoConElConsoleLog(nombre) {
+      console.log(nombre);
+      return nombre;
+    }
+  //Precedencia de valores
+  var a = 1;
+  var b = 2;
+  var c = a = b;
+  console.log (c); 
